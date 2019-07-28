f7fc21e12730bbd04dbc6296aa8db41f3d3e8514
Jun Zheng <arthurjordan@gmail.com>
2019-07-29 08:08:39 +1000

    add dart-tracks, move .git one level up

---
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -0,0 +1,20 @@
+class DiamondPrinter {
+
+
+    fun printToList(input: Char): List<String> {
+        val diamondList = mutableListOf<String>()
+
+        val diamondListLength = ('A'..'C').toList().size() * 2 - 1
+
+        intersect()
+        print a string with that length
+
+        (1..10).mapWithIndex { idx, number ->
+            diamondList.add(buildString())
+        }
+
+        fun buildString() = StringBuilder("").append("A")
+        println(diamondList)
+    }
+
+}
--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,13 +1,83 @@
-class Squares(val inputNumber: Int){
-    fun squareOfSum(): Int {
-        return (1..inputNumber).fold(0) {acc, x -> x + acc } * (1..inputNumber).fold(0) {acc, x -> x + acc }
-    }
+//class Squares(val inputNumber: Int){
+//    fun squareOfSum(): Int {
+//        return (1..inputNumber).fold(0) {acc, x -> x + acc } * (1..inputNumber).fold(0) {acc, x -> x + acc }
+//    }
+//
+//    fun sumOfSquares(): Int {
+//        return (1..inputNumber).map { it -> it * it }.sum()
+//    }
+//
+//    fun difference(): Int {
+//        return squareOfSum() - sumOfSquares()
+//    }
+//}
 
-    fun sumOfSquares(): Int {
-        return (1..inputNumber).map { it -> it * it }.sum()
-    }
+//
+//import kotlin.math.pow
+//
+////private fun Int.squared() = this * this
+//private fun Int.squared() = this.toDouble().pow(2).toInt()
+//class Squares(private val value: Int) {
+//    fun squareOfSum() = 1.rangeTo(value).sum().squared()
+//
+////    fun sumOfSquares() = 1.rangeTo(value).map(Int::squared).sum()
+////    fun sumOfSquares() = 1.rangeTo(value).sumBy(Int::squared)
+//    fun sumOfSquares() = 1.rangeTo(value).sumBy{ it.squared()}
+//    fun difference() = squareOfSum() - sumOfSquares()
+//}
 
-    fun difference(): Int {
-        return squareOfSum() - sumOfSquares()
-    }
+
+//import kotlin.math.pow
+//
+////private fun Int.square() = this * this
+//private fun Int.square() = this.toDouble().pow(2).toInt()
+//
+//class Squares (private val num: Int) {
+//    fun squareOfSum() = 1.rangeTo(num).sum().square()
+////    fun sumOfSquares() = 1.rangeTo(num).sumBy(Int::square)
+////    fun sumOfSquares() = (1..num).map{ it.square() }.sum()
+//    fun sumOfSquares() = (1..num).map { it.square() }.sum()
+//    fun difference() = squareOfSum() - sumOfSquares()
+//}
+
+
+
+//private fun Int.square() = this * this
+//
+//val intSeq = generateSequence(1) { (it + 1) }
+//val squareSeq = intSeq.map(Int::square)
+//
+//class Squares(val i: Int) {
+//
+//    val sum = { seq: Sequence<Int> -> seq.take(i).sum() }
+//
+//    fun squareOfSum() = sum(intSeq).square()
+//
+//    fun sumOfSquares() = sum(squareSeq)
+//
+//    fun difference() = squareOfSum() - sumOfSquares()
+//}
+
+
+
+private fun Int.square() = this * this
+
+class Squares (private val inputNumber: Int) {
+    fun squareOfSum() = (1..inputNumber).sum().square()
+    fun sumOfSquares() = (1..inputNumber).sumBy(Int::square)
+    fun difference() = squareOfSum() - sumOfSquares()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
