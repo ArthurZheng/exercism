eb5221ecc5d909ceb3ee550740dc2d3ee8dd8c17
Jun Zheng <arthurjordan@gmail.com>
2019-07-29 09:58:36 +1000

    more ways of doing resistor colour

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,5 +1,36 @@
 class ResistorColor {
   var colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+  final colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+  var colors = const [ 'black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white' ];
+  List<String> colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+  List<String> get colors => ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+
 
   int colorCode(String colour) => colors.indexOf(colour);
 }
+
+
+
+var colorCodes = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+};
+
+class ResistorColor {
+  List<String> colors = colorCodes.keys.toList();
+
+  int colorCode(String color) {
+    return colorCodes[color];
+  }
+}
+
+
+
