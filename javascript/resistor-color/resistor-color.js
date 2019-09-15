2179a9c1bdea5d0cb01783f5db6edcfabba00c2b
Jun Zheng <arthurjordan@gmail.com>
2019-09-15 17:46:38 +1000

    update gitignore for JS track, add resistor-color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,16 @@
+export const colorCode = (colour) => {
+  switch (colour) {
+    case 'black':
+      return 0;
+      break;
+    case 'white':
+      return 9;
+      break;
+    case 'orange':
+      return 3;
+      break;
+  }
+
+};
+
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
